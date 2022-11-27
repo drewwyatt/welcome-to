62d0018a44ebde1b512ed0de79136deaea2640ab
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-11-27 13:18:25 -0800

    landscaper

--- src/lib/data/construction-cards.ts
+++ src/lib/data/construction-cards.ts
@@ -17,7 +17,7 @@ const temp = (...houseNumbers: number[]) => houseNumbers.map(card(Effect.TempAge
 
 const deck: ConstructionCard[] = [
   ...bis(3, 4, 6, 7, 8, 9, 10, 12, 13),
-  ...landscaper(11, 14, 15, 8, 9, 1, 5, 5, 7, 8, 12, 9, 10, 2, 7, 6, 4, 11),
+  ...landscaper(1, 2, 4, 5, 5, 6, 7, 7, 8, 8, 9, 9, 10, 11, 11, 12, 14, 15),
   ...pool(4, 9, 3, 6, 8, 10, 13, 7, 12),
   ...realEstate(10, 11, 15, 2, 4, 6, 8, 5, 9, 7, 14, 5, 12, 1, 7, 9, 11, 8),
   ...surveyor(10, 14, 6, 7, 8, 9, 1, 2, 3, 5, 5, 6, 10, 11, 11, 13, 15, 8),

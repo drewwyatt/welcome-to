151706fe7dfe4212af805e683097cacd3c5ed02a
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-11-27 13:02:59 -0800

    wip

--- src/lib/data/construction-cards.ts
+++ src/lib/data/construction-cards.ts
@@ -12,52 +12,19 @@ const card = (effect: Effect) => (houseNumber: number) => ({
   houseNumber,
 })
 
-const bis = card(Effect.Bis)
-const landscaper = card(Effect.Landscaper)
-// const pool = card(Effect.PoolManufacturer)
+const bis = (...houseNumbers: number[]) => houseNumbers.map(card(Effect.Bis))
+const landscaper = (...houseNumbers: number[]) =>
+  houseNumbers.map(card(Effect.Landscaper))
 const pool = (...houseNumbers: number[]) =>
-  houseNumbers.map(houseNumber => card(Effect.PoolManufacturer)(houseNumber))
-
-// const realEstate = card(Effect.RealEstateAgent)
+  houseNumbers.map(card(Effect.PoolManufacturer))
 const realEstate = (...houseNumbers: number[]) =>
-  houseNumbers.map(houseNumber => card(Effect.RealEstateAgent)(houseNumber))
-
-// const surveyor = card(Effect.Surveyor)
-const surveyor = (...houseNumbers: number[]) =>
-  houseNumbers.map(houseNumber => card(Effect.Surveyor)(houseNumber))
-
-// const temp = card(Effect.TempAgency)
-const temp = (...houseNumbers: number[]) =>
-  houseNumbers.map(houseNumber => card(Effect.TempAgency)(houseNumber))
+  houseNumbers.map(card(Effect.RealEstateAgent))
+const surveyor = (...houseNumbers: number[]) => houseNumbers.map(card(Effect.Surveyor))
+const temp = (...houseNumbers: number[]) => houseNumbers.map(card(Effect.TempAgency))
 
 const deck: ConstructionCard[] = [
-  bis(12),
-  bis(8),
-  bis(9),
-  bis(6),
-  bis(10),
-  bis(7),
-  bis(4),
-  bis(13),
-  bis(3),
-  landscaper(11),
-  landscaper(14),
-  landscaper(15),
-  landscaper(8),
-  landscaper(9),
-  landscaper(1),
-  landscaper(5),
-  landscaper(5),
-  landscaper(7),
-  landscaper(8),
-  landscaper(12),
-  landscaper(9),
-  landscaper(10),
-  landscaper(2),
-  landscaper(7),
-  landscaper(6),
-  landscaper(4),
-  landscaper(11),
+  ...bis(12, 8, 9, 6, 10, 7, 4, 13, 3),
+  ...landscaper(11, 14, 15, 8, 9, 1, 5, 5, 7, 8, 12, 9, 10, 2, 7, 6, 4, 11),
   ...pool(4, 9, 3, 6, 8, 10, 13, 7, 12),
   ...realEstate(10, 11, 15, 2, 4, 6, 8, 5, 9, 7, 14, 5, 12, 1, 7, 9, 11, 8),
   ...surveyor(10, 14, 6, 7, 8, 9, 1, 2, 3, 5, 5, 6, 10, 11, 11, 13, 15, 8),

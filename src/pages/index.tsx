da469cc0af4c2f5bfdb12805dca950bbfb06a4bf
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-11-27 12:29:28 -0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Welcome To...</title>
+        <meta name="description" content="Virtual Deck for Welcome To..." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to...</h1>
+      </main>
+    </>
+  )
+}

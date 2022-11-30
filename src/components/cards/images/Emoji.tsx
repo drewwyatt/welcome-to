64a41ecbc89f87f37df155a026538dba47b7ba9d
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-11-30 00:01:47 -0500

    Emoji

--- src/components/cards/images/Emoji.tsx
+++ src/components/cards/images/Emoji.tsx
@@ -1,12 +1,14 @@
 import type { FC, PropsWithChildren } from 'react'
 import { classNames } from '~/lib/utils'
 
+import styles from '~/styles/Emoji.module.css'
+
 interface Props {
   className?: string
 }
 
 const Emoji: FC<PropsWithChildren<Props>> = ({ children, className }) => (
-  <i className={classNames('emoji', className)}>{children}</i>
+  <i className={classNames(styles.emoji, className)}>{children}</i>
 )
 
 export default Emoji

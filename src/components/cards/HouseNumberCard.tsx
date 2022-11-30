ffef673aaacddd70d2d6e044453c92f2f95fea41
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-11-29 23:59:16 -0500

    HouseNumberCard

--- src/components/cards/HouseNumberCard.tsx
+++ src/components/cards/HouseNumberCard.tsx
@@ -2,9 +2,11 @@ import type { FC } from 'react'
 import type { HouseNumber } from '~/lib/models'
 import Card from './Card'
 
+import styles from '~/styles/HouseNumberCard.module.css'
+
 const HouseNumberCard: FC<{ value: HouseNumber }> = ({ value }) => (
-  <Card className="house-number-card">
-    <h1>{value}</h1>
+  <Card>
+    <h1 className={styles.number}>{value}</h1>
   </Card>
 )
 

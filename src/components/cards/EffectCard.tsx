560ff50519a8a3427a9ee29e5c90751211ba72bb
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-12-02 00:36:57 -0500

    card colors

--- src/components/cards/EffectCard.tsx
+++ src/components/cards/EffectCard.tsx
@@ -1,12 +1,17 @@
 import type { FC } from 'react'
 import type { Effect } from '~/lib/models'
+import { classNames } from '~/lib/utils'
 
 import Card from './Card'
 import { emojify } from './images'
 
+import styles from '~/styles/EffectCard.module.css'
+
+const toClassName = (s: string) => s.replaceAll(' ', '')
+
 const EffectCard: FC<{ type: Effect }> = ({ type }) => (
-  <Card>
-    {emojify(type)}
+  <Card className={styles[toClassName(type)]}>
+    <picture className={styles.image}>{emojify(type)}</picture>
     <h1>{type}</h1>
   </Card>
 )

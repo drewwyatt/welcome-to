e7f32cb042be9c1101c51dcd598b725c7bf094ac
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-12-02 22:33:39 -0500

    add instructions

--- src/components/Turn.tsx
+++ src/components/Turn.tsx
@@ -48,6 +48,50 @@ const Turn: FC = () => {
             <HousingEstatePlan category={2} />
             <HousingEstatePlan category={3} />
           </div>
+          <div className={styles.instructions}>
+            <h2>Resources</h2>
+            <p>
+              App store links and the full game manual can be found at the bottom of the
+              page.
+            </p>
+            <h2>Keyboard</h2>
+            <p>You can change cards using the arrow keys and the spacebar.</p>
+            <h2>City Plans</h2>
+            <p>
+              Once a city plan has been claimed, you can click it to mark it as claimed.
+            </p>
+            <h2>Effects</h2>
+            <p>
+              <strong>{Effect.Bis}</strong> Allows the player to duplicate a house number
+              on any street. The new house must be directly adjacent, to the left or right
+              of the house with the same number.
+            </p>
+            <p>
+              <strong>{Effect.Landscaper}</strong> Allows the player to build a park. The
+              player crosses off one of the trees at the end of the street. Parks must be
+              crossed off on the same street that the house number is written
+            </p>
+            <p>
+              <strong>{Effect.PoolManufacturer}</strong> Allows a player to score points
+              when building houses that have a planned pool. If a player writes the house
+              number associated with the Pool Manufacturer effect in a house with a
+              planned pool, then the pool is built. Cross off the top-most,
+              lowest-numbered space in their pools column.
+            </p>
+            <p>
+              <strong>{Effect.RealEstateAgent}</strong> Promotes and increases the value
+              of completed housing estates.
+            </p>
+            <p>
+              <strong>{Effect.Surveyor}</strong> Allows the player to build a fence
+              between two houses on the same street.
+            </p>
+            <p>
+              <strong>{Effect.TempAgency}</strong> Allows the player to add or subtract 1
+              or 2 to the house number they are building.
+            </p>
+          </div>
+
           <button className={styles.button} onClick={redrawPlans}>
             Draw New City Plans
           </button>
